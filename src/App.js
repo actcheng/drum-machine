0cb6ecce8e1ef81bd0a27bb9ee6d4b974e584941
CT Cheng (Anna) <annacheng310@gmail.com>
2019-02-10 23:44:51 +0900

    Layout

--- src/App.js
+++ src/App.js
@@ -3,24 +3,51 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  componentDidMount () {
+          const script = document.createElement("script");
+          script.src = "https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js";
+          script.async = true;
+          document.body.appendChild(script);
+      }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div id="drum-machine">
+          <div id="display">Header 1</div>
+          <div id="pad-container">
+            <div className="drum-pad" id="Q-pad">
+                Q<audio src="audio/Heater-1.mp3" className="clip" id="Q" />
+            </div>
+            <div className="drum-pad" id="W-pad">
+                W<audio src="audio/Heater-1.mp3" className="clip" id="W" />
+            </div>
+            <div className="drum-pad" id="E-pad">
+                E<audio src="audio/Heater-1.mp3" className="clip" id="E" />
+            </div>
+            <div className="drum-pad" id="A-pad">
+                A<audio src="audio/Heater-1.mp3" className="clip" id="A" />
+            </div>
+            <div className="drum-pad" id="S-pad">
+                S<audio src="audio/Heater-1.mp3" className="clip" id="S" />
+            </div>
+            <div className="drum-pad" id="D-pad">
+                D<audio src="audio/Heater-1.mp3" className="clip" id="D" />
+            </div>
+            <div className="drum-pad" id="Z-pad">
+                Z<audio src="audio/Heater-1.mp3" className="clip" id="Z" />
+            </div>
+            <div className="drum-pad" id="X-pad">
+                X<audio src="audio/Heater-1.mp3" className="clip" id="X" />
+            </div>
+            <div className="drum-pad" id="C-pad">
+                C<audio src="audio/Heater-1.mp3" className="clip" id="C" />
+            </div>
+          </div>
+        </div>
+        <p>Drum machine by CT</p>
       </div>
+
     );
   }
 }
